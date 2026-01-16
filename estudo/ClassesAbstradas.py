3722483c05b62652454019119827e4ec6d799097
Guilherme Monteiro <guilhermemonteiroduarte851@gmail.com>
2026-01-16 11:01:36 -0300

    classes abstratas: incompleta

---
+++ estudo/ClassesAbstradas.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+
+class Fucionario(ABC):
+    def __init__(self, nome: str, salario: float) -> None:
+        self._nome = nome
+        self._salario = salario
+    
+    def __str__(self) -> str:
+        resultado = f"Nome: {self._nome}\n"
+        resultado += f"Salario: {self._salario}"
+        return resultado
+    
+    @classmethod
+    @abstractmethod
+    def calcular_salario(self) -> float:
+        pass
+    
+    @property
+    def nome(self) -> str:
+        return self._nome
+    
+    @nome.setter
+    def nome(self, nome: str) -> None:
+        self._nome = nome
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+class FucionarioCLT(Fucionario):
+    def __init__(self, nome: str, salario: float) -> None:
+        super().__init__(nome, salario)
+    
+    def calcular_salario(self):
